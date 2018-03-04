200fa2457fb7cc69680dbbe79f9c2e9ebeb247de
Andrew Bevelhymer <andrewbeav@protonmail.com>
2018-03-04 02:04:27 -0600

    convert to numpy before passing into rgb2gray

--- engine.py
+++ engine.py
@@ -10,7 +10,7 @@ def rgb2gray(rgb):
     return np.dot(rgb[...,:3], [0.299, 0.587, 0.114]).reshape(rgb.shape[0], rgb.shape[1], 1)
 
 def predict_emotion(img_str):
-    arr = rgb2gray(convert_to_array(img_str))
+    arr = rgb2gray(np.array(convert_to_array(img_str)))
     arr = arr.reshape(1,48,48,1)
 
     from keras.models import load_model
